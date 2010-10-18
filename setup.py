18f42d811620861143ed0ddbfbb0d4af433c5c3f
Ivan Gromov <ivan.gromov@redsolution.ru>
2010-10-18 14:29:54 +0600

    Fixed requirements and setup files

--- setup.py
+++ setup.py
@@ -7,13 +7,15 @@ from setuptools import setup, find_packages
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
 
 setup(
     name="redsolutioncms.django-tinymce-attachment",
-    version="0.1.0",
-    description=("Django tinymce attachment" +
-        " with RedsolutionCMS integration"),
+    version=__import__('attachment').__version__,
+    description=read('DESCRIPTION'),
     license="LGPL",
     keywords="django tinymce attachment",
 
@@ -36,11 +38,15 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
-    packages=find_packages(),
-    install_requires=['redsolutioncms.django-tinymce==1.5', 'django-imagekit==0.3.3'],
+    packages=find_packages(exclude=['example', 'example.*']),
+    install_requires=[
+        'redsolutioncms.django-tinymce==1.5',
+        'django-imagekit==0.3.3',
+        'PIL',
+    ],
     include_package_data=True,
     zip_safe=False,
-    long_description=open('README').read(),
+    long_description=read('README'),
     entry_points={
         'redsolutioncms': ['attachment = attachment.redsolution_setup', ],
     }
