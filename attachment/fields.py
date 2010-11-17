26a2934c78f6ef61f33afb1bc7b8d540b6565509
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2010-11-17 10:43:51 +0500

    Merge: Add introspection rules for django-south

--- attachment/fields.py
+++ attachment/fields.py
@@ -21,7 +21,6 @@ class ImagePreviewField(models.ImageField):
 
 try:
     from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^attachment\.fields\.ImagePreviewField"])
 except ImportError:
     pass
-else:
-    add_introspection_rules([], ["^attachment\.fields\.ImagePreviewField"])
