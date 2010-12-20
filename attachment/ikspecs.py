54dd641c2fa5cf0cf2a65fafb7dac22603cb7323
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-12-20 15:48:58 +0500

    Settings for display quality

--- attachment/ikspecs.py
+++ attachment/ikspecs.py
@@ -2,12 +2,14 @@
 
 from imagekit import processors
 from imagekit.specs import ImageSpec
+from attachment.settings import ATTACHMENT_DISPLAY_QUALITY
 
 class ResizeDisplay(processors.Resize):
     width = 800
     height = 800
 
 class Display(ImageSpec):
+    quality = ATTACHMENT_DISPLAY_QUALITY
     processors = [ResizeDisplay, ]
 
 class ThumbDisplay(processors.Resize):
