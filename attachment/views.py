59eaf412abc9ae3153d08f64159a41057e92ae4d
egor.slesarev <egor.slesarev@redsolution.ru>
2016-02-05 13:47:16 +0500

    Added class for work with watermark. Change list of images and links for TinyMCE.

--- attachment/views.py
+++ attachment/views.py
@@ -9,6 +9,7 @@ from attachment.importpath import importpath
 from attachment.models import AttachmentImage, AttachmentFile
 from django.contrib.contenttypes.models import ContentType
 
+
 def get_object(app_label, module_name, object_id=None):
     """
     Get object by app_label, module_name and object_id.
@@ -19,7 +20,7 @@ def get_object(app_label, module_name, object_id=None):
     """
     for check in [importpath(model_name)
             for model_name in settings.ATTACHMENT_FOR_MODELS]:
-        if app_label == check._meta.app_label and module_name == check._meta.module_name:
+        if app_label == check._meta.app_label and module_name == check._meta.model_name:
             model = check
             break
     else:
@@ -30,6 +31,7 @@ def get_object(app_label, module_name, object_id=None):
     else:
         return get_object_or_404(model, id=object_id)
 
+
 def images(request, app_label, module_name, object_id=None):
     object = get_object(app_label, module_name, object_id)
     if object is None:
@@ -39,8 +41,20 @@ def images(request, app_label, module_name, object_id=None):
             content_type=ContentType.objects.get_for_model(
                     object.__class__),
             object_id=object.id)
-    link_list = [(os.path.basename(obj.image.url), obj.display.url) for obj in images]
+
+    link_list = []
+    for obj in images:
+        link_list.append([os.path.basename(obj.image.url), obj.display.url])
+        for spec in settings.ATTACHMENT_SPECS_FOR_TINYMCE:
+            try:
+                img = getattr(obj, spec)
+            except AttributeError:
+                pass
+            else:
+                link_list.append([os.path.basename(img.url), img.url])
+
     return render_to_image_list(link_list)
+
 
 def links(request, app_label, module_name, object_id=None):
     try:
@@ -50,12 +64,27 @@ def links(request, app_label, module_name, object_id=None):
 
     if object is None:
         files = []
+        images = []
     else:
         files = AttachmentFile.objects.filter(
             content_type=ContentType.objects.get_for_model(
                     object.__class__),
             object_id=object.id)
+        images = AttachmentImage.objects.filter(
+            content_type=ContentType.objects.get_for_model(
+                    object.__class__),
+            object_id=object.id)
     link_list = [(os.path.basename(obj.file.url), obj.file.url) for obj in files]
+
+    for obj in images:
+        link_list.append([os.path.basename(obj.image.url), obj.display.url])
+        for spec in settings.ATTACHMENT_SPECS_FOR_TINYMCE:
+            try:
+                img = getattr(obj, spec)
+            except AttributeError:
+                pass
+            else:
+                link_list.append([os.path.basename(img.url), img.url])
 
     for model_name in settings.ATTACHMENT_LINK_MODELS:
         model = importpath(model_name)
